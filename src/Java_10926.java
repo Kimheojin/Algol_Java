e7cdf4316a525aed382d74ee14c92e01e82820ca
jin <hurjin1109@naver.com>
2024-06-25 23:12:40 +0900

    recovery_1

--- src/Java_10926.java
+++ src/Java_10926.java
@@ -1,13 +1,30 @@
-import java.util.Scanner;
+import java.io.*;
+
+import java.nio.Buffer;
+import java.util.StringTokenizer;
 
 public class Java_10926 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Scanner in = new Scanner(System.in);
+        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
+        //br.readLine 하면 한줄씩 읽어온다
 
-        String lastString = "??!";
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            StringBuilder sb = new StringBuilder();
 
-        String s = in.next();
-        System.out.println(s + lastString);
+            //빈칸으로 구분함
+            while(st.hasMoreTokens()) {
+                String Token = st.nextToken();
+                StringBuilder sb2 = new StringBuilder();
+                sb2.append(Token).reverse();
+                sb.append(sb2.toString()).append(" ");
+            }
+            System.out.println(sb);
+
+        }
+
     }
 }
